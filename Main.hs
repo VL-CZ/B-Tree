d55fc88a4d00f3f4aa446ad508f670a0d7705782
Vojtech Lengal <vojta.lengal@gmail.com>
2020-08-03 20:45:33 +0200

    treeToList added, several other changes

--- Main.hs
+++ Main.hs
@@ -1,17 +1,26 @@
-data Tree a = Null | Node a [Tree a] deriving(Show)
+data Tree a = Null | Node [a] [Tree a] deriving(Show)
 
--- treeFind :: (Ord a) => a -> Tree a -> Bool 
+treeFind :: (Ord a) => a -> Tree a -> Bool 
+treeFind value (Node values _) = value `elem` values
+treeFind _ _ = False
 
 treeAdd :: (Ord a) => a -> Tree a -> Tree a
-treeAdd toAdd Null = Node toAdd []
-treeAdd toAdd (Node value children) = 
-    let newNode = Node toAdd []
-    in Node value (newNode:children)
+treeAdd value Null = Node [value] []
+treeAdd value (Node values children) = Node (insertIntoSorted value values) children
 
 -- treeDelete :: (Ord a) => a => Tree a -> Tree a 
 
--- treeToList :: Tree a -> [a]
+treeToList :: Tree a -> [a]
+treeToList Null = []
+treeToList (Node values children) = treeToList (head children) ++ concat (zipWith (:) values (map treeToList (tail children)))
 
-treeFold :: ([b] -> a -> b) -> b -> Tree a -> b
-treeFold _ start Null = start
-treeFold f start (Node value children) = f (map (treeFold f start) children) value
+-- treeFold :: ([b] -> a -> b) -> b -> Tree a -> b
+-- treeFold _ start Null = start
+-- treeFold f start (Node value children) = f (map (treeFold f start) children) value
+
+-- insertIntoSorted value list - insert value into sorted list and return it
+insertIntoSorted :: (Ord a) => a -> [a] -> [a]
+insertIntoSorted value [] = [value]
+insertIntoSorted value (x:xs)
+    | value < x = value:x:xs
+    | otherwise = x:insertIntoSorted value xs
