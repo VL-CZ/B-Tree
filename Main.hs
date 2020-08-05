e66f167720a085290cc66cdadbd016b8320b531f
Vojtěch Lengál <vojta.lengal@gmail.com>
2020-08-05 19:24:04 +0200

    fold and tree show added

--- Main.hs
+++ Main.hs
@@ -1,4 +1,15 @@
-data Tree a = Null | Node [a] [Tree a] deriving(Show)
+data Tree a = Null | Node [a] [Tree a]
+
+instance (Show a) => Show (Tree a) where  
+    show Null = "(Null)"  
+    show (Node values children) = 
+        let
+            showedValues = map show values
+            showedChildren = map show (tail children)
+            showedFirstChild = head children
+            zippedValuesAndChildren = zip showedValues showedChildren
+        in
+            "(" ++ show showedFirstChild ++ concatMap (\x -> " " ++ fst x ++ " " ++ snd x) zippedValuesAndChildren ++ ")" 
 
 treeFind :: (Ord a) => a -> Tree a -> Bool 
 treeFind value (Node values _) = value `elem` values
@@ -14,9 +25,9 @@ treeToList :: Tree a -> [a]
 treeToList Null = []
 treeToList (Node values children) = treeToList (head children) ++ concat (zipWith (:) values (map treeToList (tail children)))
 
--- treeFold :: ([b] -> a -> b) -> b -> Tree a -> b
--- treeFold _ start Null = start
--- treeFold f start (Node value children) = f (map (treeFold f start) children) value
+treeFold :: ([b] -> [a] -> b) -> b -> Tree a -> b
+treeFold _ start Null = start
+treeFold f start (Node values children) = f (map (treeFold f start) children) values
 
 -- insertIntoSorted value list - insert value into sorted list and return it
 insertIntoSorted :: (Ord a) => a -> [a] -> [a]
