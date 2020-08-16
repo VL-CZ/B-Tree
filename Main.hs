c33633e888b2b19a9e62fcf71ddfe471c3137201
Vojtech Lengal <vojta.lengal@gmail.com>
2020-08-16 15:19:25 +0200

    added reference to parent in B-Tree data type

--- Main.hs
+++ Main.hs
@@ -1,11 +1,11 @@
 -- B-Tree data class
-data Tree a = Null | Node [a] [Tree a]
+data Tree a = Null (Tree a) | Node [a] [Tree a] (Tree a)
 
 -- show the tree
 -- prints the tree in following format: ((child0) value0 (child1) value1 ... valueN (childN+1))
 instance (Show a) => Show (Tree a) where  
-    show Null = "(Null)"  
-    show (Node values children) = 
+    show (Null _) = "(Null)"  
+    show (Node values children _) = 
         let
             showedValues = map show values
             showedChildren = map show (tail children)
@@ -17,16 +17,16 @@ instance (Show a) => Show (Tree a) where
 -- treeFind :: value -> Tree -> isInTree
 -- is this value contained in the tree
 treeFind :: (Ord a) => a -> Tree a -> Bool 
-treeFind _ Null = False
-treeFind value (Node values children)
+treeFind _ (Null _) = False
+treeFind value (Node values children _)
     | value `elem` values = True
     | otherwise = treeFind value (children !! getCountOfLowerElementsInSortedList value values)
 
 -- treeAdd :: value -> tree -> treeWithValue
 -- add the value into the tree
-treeAdd :: (Ord a) => a -> Tree a -> Tree a
-treeAdd value Null = Node [value] []
-treeAdd value (Node values children) = Node (insertIntoSorted value values) children
+-- treeAdd :: (Ord a) => a -> Tree a -> Tree a
+-- treeAdd value (Null _) = Node [value] []
+-- treeAdd value (Node values children _) = Node (insertIntoSorted value values) children
 
 -- -- treeDelete :: value -> tree -> treeWithoutValue
 -- treeDelete :: (Ord a) => a => Tree a -> Tree a 
@@ -34,14 +34,14 @@ treeAdd value (Node values children) = Node (insertIntoSorted value values) chil
 -- treeToList :: tree -> list
 -- convert tree into list (values are ordered)
 treeToList :: Tree a -> [a]
-treeToList Null = []
-treeToList (Node values children) = treeToList (head children) ++ concat (zipWith (:) values (map treeToList (tail children)))
+treeToList (Null _) = []
+treeToList (Node values children _) = treeToList (head children) ++ concat (zipWith (:) values (map treeToList (tail children)))
 
 -- treeFold :: f -> start -> tree -> result
 -- fold the tree
 treeFold :: ([b] -> [a] -> b) -> b -> Tree a -> b
-treeFold _ start Null = start
-treeFold f start (Node values children) = f (map (treeFold f start) children) values
+treeFold _ start (Null _) = start
+treeFold f start (Node values children _) = f (map (treeFold f start) children) values
 
 -- insertIntoSorted :: value -> list -> result 
 -- insert value into sorted list and return it
